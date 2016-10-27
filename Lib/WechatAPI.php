ebf100a69d1dd21b5e05e6d01b11cab0c15da422
Demon Zhang <demon.zhang@samesamechina.com>
2016-10-27 11:06:25 +0800

    no message

--- Lib/WechatAPI.php
+++ Lib/WechatAPI.php
@@ -48,19 +48,19 @@ class WechatAPI {
     $api_url = 'http://coach.samesamechina.com/v2/wx/card/js/add/json?access_token='. CURIO_TOKEN;
     // 参数数组
     $data[] = array(
-            'card_id' => 'pKCDxjjMu_A_OlyidjNNeQ2Hqdrk',
+            'card_id' => 'pKCDxjudoAbRiE0J-rnGg48emKIA',
       //'card_id' => 'pKCDxjmRzfE5XdnYkXaZS1iAaCHQ',
             'code' => '',
             'openid' => ''
     );
     $data[] = array(
-            'card_id' => 'pKCDxjjMu_A_OlyidjNNeQ2Hqdrk',
+            'card_id' => 'pKCDxjoOSPBiTasJ18-vUUdjOiVo',
       //'card_id' => 'pKCDxjmRzfE5XdnYkXaZS1iAaCHQ',
             'code' => '',
             'openid' => ''
     );
     $data[] = array(
-            'card_id' => 'pKCDxjjMu_A_OlyidjNNeQ2Hqdrk',
+            'card_id' => 'pKCDxjlYWmhhXaOVauWoQVzT2zdY',
       //'card_id' => 'pKCDxjmRzfE5XdnYkXaZS1iAaCHQ',
             'code' => '',
             'openid' => ''
